54426077a301c1e1dd20691aa4b1ce915a41a8f4
Jonas Busk <jonasbusk@gmail.com>
2021-01-16 09:26:23 +0100

    update layout

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
+import Tab from 'react-bootstrap/Tab'
 import Table from 'react-bootstrap/Table'
+import Tabs from 'react-bootstrap/Tabs'
 import Form from 'react-bootstrap/Form'
 
 import rosters from '../data'
@@ -59,97 +59,100 @@ class Team extends Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col>
-            <Accordion>
-              <Card>
-                <Accordion.Toggle as={Card.Header} className="budget-header" eventKey="0">
-                  Budget <span className="caret"></span>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <Form inline>
-                      Budget: <Form.Control type="number" size="sm" className="text-right" value={this.state.budget} onChange={(e) => this.setState({budget: e.target.value})} />GP
-                    </Form>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-          </Col>
-        </Row>
-        <Row>
-          <Col md="5">
-            <Table borderless size="sm" className="margin-zero team-table-1">
-              <tbody>
-                <tr>
-                  <td>Team Name:</td>
-                  <td><Form.Control type="text" size="sm" className="text-center" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} /></td>
-                </tr>
-                <tr>
-                  <td>Team Roster:</td>
-                  <td>
-                    <Form.Control as="select" size="sm" onChange={(e) => this.setRoster(e.target.value)}>
-                     {rosters.map((roster, i) => {return <option key={i} value={i}>{roster.name}</option>})}
-                    </Form.Control>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Coach:</td>
-                  <td><Form.Control type="text" size="sm" className="text-center" value={this.state.coach} onChange={(e) => this.setState({coach: e.target.value})} /></td>
-                </tr>
-                <tr>
-                  <td>Treasury:</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.getTreasury())} readOnly /></td>
-                </tr>
-                <tr>
-                  <td>Team Value:</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.getTeamValue())} readOnly /></td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-          <Col md="7">
-            <Table borderless size="sm" className="margin-zero team-table-2">
-              <tbody>
-                <tr>
-                  <td>Team re-rolls:</td>
-                  <td><Form.Control type="number" size="sm" value={this.state.reRolls} onChange={(e) => this.setState({reRolls: Math.min(Math.max(parseInt(e.target.value) || 0, 0), 8)})} /></td>
-                  <td>x</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.roster.reRollsCost)} readOnly /></td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.reRolls * this.state.roster.reRollsCost)} readOnly /></td>
-                </tr>
-                <tr>
-                  <td>Dedicated Fans:</td>
-                  <td><Form.Control type="number" size="sm" value={this.state.dedicatedFans} onChange={(e) => this.setState({dedicatedFans: Math.max(parseInt(e.target.value) || 0, 0)})} /></td>
-                  <td>x</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(10000)} plaintext readOnly /></td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.dedicatedFans * 10000)} readOnly /></td>
-                </tr>
-                <tr>
-                  <td>Assistant Coaches:</td>
-                  <td><Form.Control type="number" size="sm" value={this.state.assistantCoaches} onChange={(e) => this.setState({assistantCoaches: Math.max(parseInt(e.target.value) || 0, 0)})} /></td>
-                  <td>x</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(10000)} plaintext readOnly /></td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.assistantCoaches * 10000)} readOnly /></td>
-                </tr>
-                <tr>
-                  <td>Cheerleaders:</td>
-                  <td><Form.Control type="number" size="sm" value={this.state.cheerleaders} onChange={(e) => this.setState({cheerleaders: Math.max(parseInt(e.target.value) || 0, 0)})} /></td>
-                  <td>x</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(10000)} plaintext readOnly /></td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.cheerleaders * 10000)} readOnly /></td>
-                </tr>
-                <tr>
-                  <td>Apothecary:</td>
-                  <td><Form.Control type="number" size="sm" value={this.state.apothecary} onChange={(e) => this.setState({apothecary: Math.min(Math.max(parseInt(e.target.value) || 0, 0), 1)})} readOnly={!this.state.roster.apothecaryAllowed} /></td>
-                  <td>x</td>
-                  <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(50000)} plaintext readOnly /></td>
-                  <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.apothecary * 50000)} readOnly /></td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
+
+        <Tabs defaultActiveKey="team">
+          <Tab eventKey="team" title="Team">
+            <Row>
+              <Col md="5">
+                <Table borderless size="sm" className="margin-zero team-table-1">
+                  <tbody>
+                    <tr>
+                      <td>Team Name:</td>
+                      <td><Form.Control type="text" size="sm" className="text-center" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})} /></td>
+                    </tr>
+                    <tr>
+                      <td>Team Roster:</td>
+                      <td>
+                        <Form.Control as="select" size="sm" onChange={(e) => this.setRoster(e.target.value)}>
+                         {rosters.map((roster, i) => {return <option key={i} value={i}>{roster.name}</option>})}
+                        </Form.Control>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Coach:</td>
+                      <td><Form.Control type="text" size="sm" className="text-center" value={this.state.coach} onChange={(e) => this.setState({coach: e.target.value})} /></td>
+                    </tr>
+                    <tr>
+                      <td>Treasury:</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.getTreasury())} readOnly /></td>
+                    </tr>
+                    <tr>
+                      <td>Team Value:</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.getTeamValue())} readOnly /></td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Col>
+              <Col md="7">
+                <Table borderless size="sm" className="margin-zero team-table-2">
+                  <tbody>
+                    <tr>
+                      <td>Team re-rolls:</td>
+                      <td><Form.Control type="number" size="sm" value={this.state.reRolls} onChange={(e) => this.setState({reRolls: Math.min(Math.max(parseInt(e.target.value) || 0, 0), 8)})} /></td>
+                      <td>x</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.roster.reRollsCost)} readOnly /></td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.reRolls * this.state.roster.reRollsCost)} readOnly /></td>
+                    </tr>
+                    <tr>
+                      <td>Dedicated Fans:</td>
+                      <td><Form.Control type="number" size="sm" value={this.state.dedicatedFans} onChange={(e) => this.setState({dedicatedFans: Math.max(parseInt(e.target.value) || 0, 0)})} /></td>
+                      <td>x</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(10000)} plaintext readOnly /></td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.dedicatedFans * 10000)} readOnly /></td>
+                    </tr>
+                    <tr>
+                      <td>Assistant Coaches:</td>
+                      <td><Form.Control type="number" size="sm" value={this.state.assistantCoaches} onChange={(e) => this.setState({assistantCoaches: Math.max(parseInt(e.target.value) || 0, 0)})} /></td>
+                      <td>x</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(10000)} plaintext readOnly /></td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.assistantCoaches * 10000)} readOnly /></td>
+                    </tr>
+                    <tr>
+                      <td>Cheerleaders:</td>
+                      <td><Form.Control type="number" size="sm" value={this.state.cheerleaders} onChange={(e) => this.setState({cheerleaders: Math.max(parseInt(e.target.value) || 0, 0)})} /></td>
+                      <td>x</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(10000)} plaintext readOnly /></td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.cheerleaders * 10000)} readOnly /></td>
+                    </tr>
+                    <tr>
+                      <td>Apothecary:</td>
+                      <td><Form.Control type="number" size="sm" value={this.state.apothecary} onChange={(e) => this.setState({apothecary: Math.min(Math.max(parseInt(e.target.value) || 0, 0), 1)})} readOnly={!this.state.roster.apothecaryAllowed} /></td>
+                      <td>x</td>
+                      <td><Form.Control type="text" size="sm" className="text-right" defaultValue={this.formatCost(50000)} plaintext readOnly /></td>
+                      <td><Form.Control type="text" size="sm" className="text-right" value={this.formatCost(this.state.apothecary * 50000)} readOnly /></td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Tab>
+          <Tab eventKey="budget" title="Budget">
+            <Row>
+              <Col md="5">
+                <Table borderless size="sm" className="margin-zero budget-table">
+                  <tbody>
+                    <tr>
+                      <td>Budget:</td>
+                      <td><Form.Control type="number" size="sm" className="text-right" value={this.state.budget} onChange={(e) => this.setState({budget: e.target.value})} /></td>
+                      <td>GP</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Tab>
+        </Tabs>
+
         <Row>
           <Col>
             <Table bordered hover size="sm" className="player-table table-striped">
@@ -188,6 +191,7 @@ class Team extends Component {
             </Table>
           </Col>
         </Row>
+
       </Container>
     )
   }
